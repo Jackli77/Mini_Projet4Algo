2ade04f44fbc93491ea11ce85f978a92e2ac929b
locolinet <louis.colinet@student.uclouvain.be>
2020-10-06 14:14:13 +0200

    InsertionSort

---
+++ src/InsertionSort.java
@@ -0,0 +1,17 @@
+// Louis Colinet
+
+public class InsertionSort {
+
+    public static void sort(int[] a){
+        int l = a.length;
+        for(int i = 0; i < l; i++){
+            int x = a[i];
+            int j = i;
+            while(j > 0 && a[j-1] > x){
+                a[j] = a[j-1];
+                j--;
+            }
+            a[j] = x;
+        }
+    }
+}
